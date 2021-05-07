c9e18d45301d072914e199d394312f8c74eb7b7c
Антон Крестьянинов <9593795@gmail.com>
2021-05-07 16:08:39 +0300

    Исправлена опечатка notifications_channel -> chat_channel

--- app/services/chat_broadcaster.rb
+++ app/services/chat_broadcaster.rb
@@ -2,6 +2,6 @@
 
 module ChatBroadcaster
   def self.call(recipient_id, text)
-    ActionCable.server.broadcast "notifications_channel_#{recipient_id}", text: text
+    ActionCable.server.broadcast "chat_channel_#{recipient_id}", text: text
   end
 end
